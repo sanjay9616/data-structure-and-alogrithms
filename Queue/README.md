7a385a47335a41439a478a17afb492cfbb3caa0f
sanjaymoglix <sanjay.kumar@moglix.com>
2024-04-01 22:04:10 +0530

    --

--- Queue/README.md
+++ Queue/README.md
@@ -79,7 +79,26 @@ queue.deQueue()
 print(queue.getFront(), queue.getRear(), queue.displayQueue())
 queue.deQueue()
 print(queue.getFront(), queue.getRear(), queue.displayQueue())
+```
 
+**Implementation using queue.Queue in Python**
+
+```python
+from queue import Queue
+q = Queue(maxsize=3) # Number of items allowed in the queue.
+print(q.qsize())
+q.put('a') # Put an item into the queue. If the queue is full, wait until a free slot is available before adding the item.
+q.put('b') # Return the number of items in the queue.
+q.put('c')
+print("\nFull: ", q.full())
+print("\nElements dequeued from the queue")
+print(q.get()) # Remove and return an item from the queue. If queue is empty, wait until an item is available.
+print(q.get())
+print(q.get())
+print("\nEmpty: ", q.empty()) # Return True if the queue is empty, False otherwise.
+q.put(1)
+print("\nEmpty: ", q.empty())
+print("Full: ", q.full()) # Return True if there are maxsize items in the queue. If the queue was initialized with maxsize=0 (the default), then full() never returns True.
 ```
 
 <h2><a href="https://github.com/sanjay9616/data-structure-and-alogrithms/blob/master/README.md"> 🔙 Back</a></h2>
--- Sorting/Merge sort/demo.py
+++ Sorting/Merge sort/demo.py
@@ -1,63 +1,15 @@
-class Queue:
-    def __init__(self, capacity):
-        self.queue = [None] * capacity
-        self.front = 0
-        self.rear = capacity - 1
-        self.size = 0
-        self.capacity = capacity
-
-    def isFull(self):
-        return self.size == self.capacity
-
-    def isEmpty(self):
-        return self.size == 0
-
-    def enQueue(self, data):
-        if (self.isFull()):
-            print("Queue is full.")
-            return
-        self.rear = (self.rear + 1) % (self.capacity)
-        self.queue[self.rear] = data
-        self.size = self.size + 1
-
-    def deQueue(self):
-        if (self.isEmpty()):
-            print("Queue is empty")
-            return
-        x = self.queue[self.front]
-        self.front = (self.front + 1) % (self.capacity)
-        self.size = self.size - 1
-        return x
-
-    def getFront(self):
-        if (self.isEmpty()):
-            print("Queue is empty")
-            return
-        return self.queue[self.front]
-
-    def getRear(self):
-        if (self.isEmpty()):
-            print("Queue is empty")
-            return
-        return self.queue[self.rear]
-
-    def displayQueue(self):
-        if (self.isEmpty()):
-            print("Queue is empty")
-            return
-        return self.queue[self.front: self.rear + 1]
-
-
-queue = Queue(5)
-queue.enQueue(1)
-print(queue.getFront(), queue.getRear(), queue.displayQueue())
-queue.enQueue(2)
-print(queue.getFront(), queue.getRear(), queue.displayQueue())
-queue.enQueue(3)
-print(queue.getFront(), queue.getRear(), queue.displayQueue())
-queue.deQueue()
-print(queue.getFront(), queue.getRear(), queue.displayQueue())
-queue.deQueue()
-print(queue.getFront(), queue.getRear(), queue.displayQueue())
-queue.deQueue()
-print(queue.getFront(), queue.getRear(), queue.displayQueue())
+from queue import Queue
+q = Queue(maxsize=3) # Number of items allowed in the queue.
+print(q.qsize())
+q.put('a') # Put an item into the queue. If the queue is full, wait until a free slot is available before adding the item.
+q.put('b') # Return the number of items in the queue.
+q.put('c')
+print("\nFull: ", q.full())
+print("\nElements dequeued from the queue")
+print(q.get()) # Remove and return an item from the queue. If queue is empty, wait until an item is available.
+print(q.get())
+print(q.get())
+print("\nEmpty: ", q.empty()) # Return True if the queue is empty, False otherwise.
+q.put(1)
+print("\nEmpty: ", q.empty())
+print("Full: ", q.full()) # Return True if there are maxsize items in the queue. If the queue was initialized with maxsize=0 (the default), then full() never returns True.
